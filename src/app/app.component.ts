e396c591d2c3739d4299589f0896a1ce1f1d9add
DLmar <dlmar2code@gmail.com>
2023-06-21 18:32:00 +0300

    resolved test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-password-strength-app';
+  password: string = '';
+  passwordStrength: string = '';
+
+  getPasswordColor(index: number) {
+    const passwordLength = this.password.length;
+
+    if (passwordLength === 0) {
+      return 'gray';
+    } else if (passwordLength < 8) {
+      return 'red';
+    }
+
+    const hasLetters = /[a-zA-Z]/.test(this.password);
+    const hasDigits = /\d/.test(this.password);
+    const hasSymbols = /[!@#$%^&*]/.test(this.password);
+
+    if (
+      (hasLetters && !hasDigits && !hasSymbols) ||
+      (!hasLetters && hasDigits && !hasSymbols) ||
+      (!hasLetters && !hasDigits && hasSymbols)
+    ) {
+      return index === 0 ? 'red' : 'gray';
+    }
+
+    if (
+      (hasLetters && hasDigits && !hasSymbols) ||
+      (hasLetters && !hasDigits && hasSymbols) ||
+      (!hasLetters && hasDigits && hasSymbols)
+    ) {
+      return index === 0 || index === 1 ? 'yellow' : 'gray';
+    }
+
+    if (hasLetters && hasDigits && hasSymbols) {
+      return 'green';
+    }
+
+    return 'gray';
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
